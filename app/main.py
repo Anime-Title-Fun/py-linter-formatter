6fd51d75f251e72e52e76d8b04bcea4a90644a9d
deathhanthhem-web <deathhanthhem@gmail.com>
2026-08-27 18:15:49 +0200

    Restore project structure

---
+++ app/main.py
@@ -0,0 +1,17 @@
+def format_linter_error(error):
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+def format_single_linter_file(file_path, errors):
+    return {
+        "path": file_path,
+        "errors": [format_linter_error(e) for e in errors],
+        "status": "failed" if errors else "passed"
+    }
+def format_linter_report(linter_report):
+    return [format_single_linter_file(path, errs) for path, errs in linter_report.items()]
+
